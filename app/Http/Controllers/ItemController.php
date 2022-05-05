2d995f9b35796001b9902aefa943dc6f31f2fe08
Your Name <you@example.com>
2022-05-05 15:02:41 +0200

    done with html

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\TrainTrip;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
@@ -13,7 +14,12 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        TrainTrip::OrderBy('Departure-Time', 'DESC')->get();
+        $trips = TrainTrip::all();
+        $data = [
+            'trips' => $trips
+        ];
+        return view('home', $data);
     }
 
     /**
